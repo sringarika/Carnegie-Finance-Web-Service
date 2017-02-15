2c5a59c980c1b67edfcfa09183d49b1495221c4a
Yuchen Shi <yuchenshi@cmu.edu>
2017-02-14 22:44:24 -0500

    Add a global error handler and handle /login error as specified.

--- cfsws/src/main/java/fund/mymutual/cfsws/rest/SessionController.java
+++ cfsws/src/main/java/fund/mymutual/cfsws/rest/SessionController.java
@@ -2,17 +2,14 @@ package fund.mymutual.cfsws.rest;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import fund.mymutual.cfsws.business.SessionService;
-import fund.mymutual.cfsws.model.User;
 
 @RestController
 public class SessionController {
@@ -24,23 +21,6 @@ public class SessionController {
     @RequestMapping(value="/greeting", method=RequestMethod.GET)
     public MessageDTO greeting() {
         return new MessageDTO("Hello from CFS!");
-    }
-
-    @RequestMapping(value="/login", method=RequestMethod.POST)
-    public MessageDTO login(@Valid @RequestBody LoginDTO login, HttpServletResponse response) {
-        User user = sessionService.authenticateUser(login.getUsername(), login.getPassword());
-        if (user == null) { // Failure case
-            return new MessageDTO("There seems to be an issue with the username/password combination that you entered");
-        } else { // Successful login
-            String authToken = sessionService.beginSession(user.getUsername());
-            Cookie cookie = new Cookie(AUTH_COOKIE, authToken);
-            cookie.setMaxAge(60 * 60 * 24);
-            // TODO: Make the cookie expire after remaining idle for 15 minutes.
-            // This basically means the cookie should be refreshed every time a response is sent.
-            response.addCookie(cookie);
-            String firstName = user.getFirstName();
-            return new MessageDTO("Welcome " + firstName);
-        }
     }
 
     @RequestMapping(value="/logout", method=RequestMethod.POST)
